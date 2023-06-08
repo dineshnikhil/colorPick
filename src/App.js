5ef93f64dc9641132ed0297895ad99164a1a961f
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-06-08 11:40:41 +0530

    created the footer and the responsive design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 
@@ -9,6 +10,7 @@ function App() {
 			<main>
 				<Home />
 			</main>
+			<Footer />
 		</div>
 	);
 }
